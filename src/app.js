cef964a788347e92d46810f79f93ba276dd695e9
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-23 20:35:25 -0400

    Basic set up with app.js and gitignore

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}...`);
+});
