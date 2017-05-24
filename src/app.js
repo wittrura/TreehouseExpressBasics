d742f5f6026c6a306855fe3d6adcb07c67ea5529
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-23 21:45:22 -0400

    Create route for blog post and add mock data

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const POSTS = require('./mock/posts.json');
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/blog', function(req, res) {
+  res.send(POSTS);
 });
 
 app.listen(PORT, function() {
