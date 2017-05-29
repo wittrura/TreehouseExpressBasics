af9ffff7af6357642e7b5d6300958bdd217d9652
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-29 09:37:00 -0400

    Install and setup for pug, creates index template

--- src/app.js
+++ src/app.js
@@ -2,17 +2,29 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
+const PORT = 3000;
 const POSTS = require('./mock/posts.json');
 
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/templates');
+
+
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.render('index', { title: 'Hey', message: 'Hell there!' });
 });
 
-app.get('/blog', function(req, res) {
-  res.send(POSTS);
+app.get('/blog/:title?', function(req, res) {
+  var title = req.params.title;
+
+  if (title === undefined) {
+    res.status(503);
+    res.send("This page is under construction!");
+  } else {
+    var post = POSTS[title];
+    res.send(post);
+  }
 });
 
 app.listen(PORT, function() {
